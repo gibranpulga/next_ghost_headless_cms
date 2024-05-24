5d6926f5a24f6576bcae0a89d5611614c8e6b621
Gibran Antonio Pulga <gibranpulga@gmail.com>
2024-05-24 19:45:54 -0300

    GA4 e ads.txt

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      domains: ["images.unsplash.com", "static.ghost.org", "www.gravatar.com", "localhost", "ghost-test-2.ghost.io"]
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  }
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["images.unsplash.com", "static.ghost.org", "www.gravatar.com", "localhost", "ghost-test-2.ghost.io"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: '/public/ads.txt',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="light" lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8MGDX8XKCN"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8MGDX8XKCN');
+            `,
+          }}
+        />
+      </Head>
       <body className="bg-[--bg-color] dark:bg-gray-900">
         <Main />
         <NextScript />
