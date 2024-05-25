28c8f9f2c6e18a5f14f451ce2cfee2a7e94b8030
Gibran Antonio Pulga <gibranpulga@gmail.com>
2024-05-24 21:30:21 -0300

    Meta pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className="light" lang="en">
       <Head>
-        {/* Google tag (gtag.js) */}
+        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-8MGDX8XKCN"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -16,6 +16,32 @@ export default function Document() {
             `,
           }}
         />
+
+        {/* Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '975476734235708');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=975476734235708&ev=PageView&noscript=1"
+          />
+        </noscript>
       </Head>
       <body className="bg-[--bg-color] dark:bg-gray-900">
         <Main />
