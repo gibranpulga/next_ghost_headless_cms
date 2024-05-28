326754d8c277431a4518e9f86a08798fc5bad4b2
Gibran Antonio Pulga <gibranpulga@gmail.com>
2024-05-28 13:56:17 -0300

    Add ads.txt

--- app/Header.tsx
+++ app/Header.tsx
@@ -7,9 +7,6 @@ import type { Settings } from "@tryghost/content-api";
 function Header({ settings }: { settings: Settings }) {
   return (
     <>
-      <Head>
-        {/* Google Analytics and Facebook Pixel scripts are already included in _document.tsx */}
-      </Head>
       <header className="px-2 sm:px-4 py-2.5 w-full" style={{ backgroundColor: "white" }}>
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           {/* Center: Logo */}
